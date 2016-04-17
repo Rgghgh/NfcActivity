2f5355b90ebad99dbad61e485027adae19ff6631
Rgghgh <tomer1091@gmail.com>
2016-04-17 13:21:25 +0300

    Usage example for NfcConnection & NfcTester.

--- app/src/main/java/com/rgghgh/nfcactivitydemo/MainActivity.java
+++ app/src/main/java/com/rgghgh/nfcactivitydemo/MainActivity.java
@@ -2,9 +2,12 @@ package com.rgghgh.nfcactivitydemo;
 
 import android.nfc.FormatException;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.rgghgh.nfcactivity.NfcActivity;
 import com.rgghgh.nfcactivity.NfcConnection;
+import com.rgghgh.nfcactivity.NfcTester;
 import com.rgghgh.nfcactivity.exceptions.NfcNotWritableException;
 import com.rgghgh.nfcactivity.exceptions.NfcTextTooLargeException;
 
@@ -12,12 +15,22 @@ import java.io.IOException;
 
 public class MainActivity extends NfcActivity
 {
+    private TextView tvDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvDisplay = (TextView) findViewById(R.id.tvDisplay);
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        runNfcTest(); // see below
     }
 
     @Override
@@ -25,24 +38,51 @@ public class MainActivity extends NfcActivity
     {
         try
         {
-            // get unique tag id
-            String id = conn.getTagId();
+            String id = conn.getTagId(); // get unique tag id
+            String data = conn.read(); // get string data from tag
 
-            // get string data from tag
-            String data = conn.read();
+            if(!data.isEmpty())
+                setDisplay(id + " - " + data);
 
-            // write string data to tag
-            conn.write("Hello, World!");
-
-            // write string data to tag + application record
-            conn.write("Hello, World!", this);
-
-            // make tag "read only"
-            conn.makeReadOnly();
+            conn.write("Hello, World!"); // write string data to tag
+            conn.write("Hello, World!", this); // write string data to tag + application record
+            conn.makeReadOnly(); // make tag "read only"
         }
-        catch (FormatException | IOException | NfcNotWritableException | NfcTextTooLargeException e)
+        catch (FormatException e)
         {
-            e.printStackTrace();
+            setDisplay("The data on this tag is malformed.");
         }
+        catch (IOException e)
+        {
+            setDisplay("The tag was disconnected mid-action");
+        }
+        catch (NfcNotWritableException e)
+        {
+            setDisplay("Nfc tag is set to 'Read Only' and can not be changed.");
+        }
+        catch (NfcTextTooLargeException e)
+        {
+            setDisplay("This data is too large for this tag, try another tag or smaller data.");
+        }
+    }
+
+    public void runNfcTest()
+    {
+        // NfcTester object usage:
+        NfcTester tester = new NfcTester(this);
+
+        if(!tester.hasNfc()) // same as this.hasNfc()
+            setDisplay("Device is not NFC compatible.");
+
+        if(!tester.isNfcEnabled()) // same as this.isNfcEnabled()
+            setDisplay("NFC is disabled. Please enable in settings.");
+
+        if(!tester.hasNfcPermission()) // same as this.hasNfcPermission()
+            setDisplay("NFC usage permission has not been granted.");
+    }
+
+    public void setDisplay(String text)
+    {
+        this.tvDisplay.setText(text);
     }
 }
