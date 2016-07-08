9fb9373316930e7d8b7f589d3e0430b056edce79
Rgghgh <tomer1091@gmail.com>
2016-07-08 17:56:05 +0300

    AppCompatActivity Support, new NfcBeamActivity, Updated gradle.

---
+++ nfcactivity/src/main/java/com/rgghgh/nfcactivity/BeamAppCompatActivity.java
@@ -0,0 +1,115 @@
+package com.rgghgh.nfcactivity;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcAdapter.CreateNdefMessageCallback;
+import android.nfc.NfcEvent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Extend this in your activity to implement Android Beam functionality.
+ * @since v1.0
+ */
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+public abstract class BeamAppCompatActivity extends AppCompatActivity implements CreateNdefMessageCallback
+{
+    private NfcAdapter adapter;
+    private boolean bound;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        this.adapter = NfcAdapter.getDefaultAdapter(this);
+        this.adapter.setNdefPushMessageCallback(this, this);
+        this.bound = false;
+    }
+
+    /**
+     * Called on android beam contact.
+     * Physically when two phones touch.
+     * This method determines what data should be sent from this device to the other.
+     * Data will be sent from this device if on cantact, when the beam animation is shown,
+     * the user of the current device clicks the animation screen.
+     * @return data to be passed to the contacted phone, should the user will invoke the beam from this phone.
+     * @since v1.0
+     */
+    public abstract String onBeamSend();
+
+    /**
+     * Called on android beam data received.
+     * @param data data message from the android beam device.
+     * @since v1.0
+     */
+    public abstract void onBeamReceived(String data);
+
+    @Override
+    public final NdefMessage createNdefMessage(NfcEvent event)
+    {
+        if (this.bound)
+            return new NdefMessage(new NdefRecord[]{
+                    NdefRecord.createTextRecord("en", onBeamSend()),
+                    NdefRecord.createApplicationRecord(getApplicationInfo().packageName)
+            });
+        return new NdefMessage(new NdefRecord[]{
+                NdefRecord.createTextRecord("en", onBeamSend())
+        });
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction()))
+        {
+            Intent intent = getIntent();
+            NdefMessage msg = (NdefMessage) intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)[0];
+            String data = new String(msg.getRecords()[0].getPayload());
+            onBeamReceived(data);
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent)
+    {
+        setIntent(intent);
+    }
+
+    /**
+     * Manually invoke Android Beam to share data.
+     * @since v1.0
+     */
+    public void invokeBeam()
+    {
+        this.adapter.invokeBeam(this);
+    }
+
+    /**
+     * Beams bound to app will be read only by this app. if it not open, it will be.
+     * if it is not installed, the app's page on the google play store will be opened.
+     * @return true if the future beams sent will be bound to the app. false otherwise.
+     * @since v1.0
+     */
+    public final boolean isBoundToApp()
+    {
+        return this.bound;
+    }
+
+    /**
+     * Sets the beam bound to app state.
+     * Beams bound to app will be read only by this app. if it not open, it will be.
+     * if it is not installed, the app's page on the google play store will be opened.
+     * @param state bind to app.
+     * @since v1.0
+     */
+    public final void setBoundToApp(boolean state)
+    {
+        this.bound = state;
+    }
+
+}
---
+++ nfcactivity/src/main/java/com/rgghgh/nfcactivity/NfcBeamActivity.java
@@ -0,0 +1,130 @@
+package com.rgghgh.nfcactivity;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.MifareUltralight;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NfcA;
+import android.os.Bundle;
+
+/**
+ * Extend this class in your activity to implement NFC communication functionality.
+ * @since v1.0
+ */
+public abstract class NfcBeamActivity extends BeamActivity
+{
+    private NfcAdapter adapter;
+    private NfcTester tester;
+    private PendingIntent pendingIntent;
+    private IntentFilter[] intentFiltersArray;
+    private String[][] techListsArray;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        this.adapter = NfcAdapter.getDefaultAdapter(this);
+        this.tester = new NfcTester(this);
+        this.pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter ndefFilter = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        ndefFilter.addAction(NfcAdapter.ACTION_TECH_DISCOVERED);
+
+        try
+        {
+            ndefFilter.addDataType("*/*");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        this.intentFiltersArray = new IntentFilter[]{ndefFilter};
+        this.techListsArray = new String[][]{
+                new String[]{
+                        NfcA.class.getName(),
+                        Ndef.class.getName(),
+                        MifareUltralight.class.getName()
+                },
+                new String[]{
+                        NfcA.class.getName()
+                },
+                new String[]{
+                        Ndef.class.getName()
+                },
+                new String[]{
+                        MifareUltralight.class.getName()
+                }
+        };
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (hasNfc())
+            this.adapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (hasNfc())
+            this.adapter.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, techListsArray);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent)
+    {
+        Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        if (tagFromIntent != null)
+        {
+            NfcConnection conn = new NfcConnection(tagFromIntent);
+            onNfcStart(conn);
+        }
+    }
+
+    /**
+     * <p>Invoked on Nfc tag contacted, <b>before</b> any data is tranfered.</p>
+     * <p>Actions can be performed on the Nfc Tag using the NfcConnnection param.</p>
+     * @param conn NfcConnection with the tag contacted.
+     * @see NfcConnection
+     * @since v1.0
+     */
+    public abstract void onNfcStart(NfcConnection conn);
+
+    /**
+     * <p>Checks if device is Nfc compatible.</p>
+     * @return true if compatible, false otherwise.
+     * @since v1.0
+     */
+    public final boolean hasNfc()
+    {
+        return this.tester.hasNfc();
+    }
+
+    /**
+     * <p>Checks if app has Nfc Permission enabled.</p>
+     * @return true if permission is enabled, false otherwise.
+     * @see NfcTester
+     * @since v1.0
+     */
+    public final boolean hasNfcPermission()
+    {
+        return this.tester.hasNfcPermission();
+    }
+
+    /**
+     * <p>Checks if phone has Nfc hardware enabled.</p>
+     * @return true if hardware is enabled, false otherwise.
+     * @see NfcTester
+     * @since v1.0
+     */
+    public final boolean isNfcEnabled()
+    {
+        return this.tester.isNfcEnabled();
+    }
+}
